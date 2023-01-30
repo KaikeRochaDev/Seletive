f459931a03db24215ad5d9d9ae3950275cc28103
KaikeRochaDev <kaikerochadev@gmail.com>
2023-01-30 11:33:38 -0300

    accessing a vacancy

--- vagas/views.py
+++ vagas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import Http404
 from empresa.models import Vagas
 from django.contrib import messages
@@ -37,3 +37,7 @@ def nova_vaga(request):
         return redirect(f'/home/empresa/{empresa}')
     elif request.method == "GET":
         raise Http404()
+
+def vaga(request, id):
+    vaga = get_object_or_404(Vagas, id=id)
+    return render(request, 'vaga.html', {'vaga': vaga})
