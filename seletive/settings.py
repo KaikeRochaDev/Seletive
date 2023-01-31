9bdc9f2c4625fdf2f3548ce46bb0ca06c4338ac5
KaikeRochaDev <kaikerochadev@gmail.com>
2023-01-31 16:19:03 -0300

    sending emails

--- seletive/settings.py
+++ seletive/settings.py
@@ -145,3 +145,9 @@ MESSAGE_TAGS = {
     constants.INFO: 'alert-info',
     constants.WARNING: 'alert-warning',
 }
+
+
+# Email
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST_USER = "kaikerochadev@gmail.com"
